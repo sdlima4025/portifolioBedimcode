f699cc8e8424948279843e1e0088de99ba4d96b2
Sérgio Lima <sergiolimafg3@gmail.com>
2023-02-23 20:27:04 -0300

    portifolio inicializado criado menu hamburguer com efeito hover.

---
+++ assets/js/main.js
@@ -0,0 +1,61 @@
+/* =====MENU SHOW===== */
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+        if(toggle && nav){
+            toggle.addEventListener('click', ()=> {
+                nav.classList.toggle('show')
+            })
+        }
+}
+showMenu('nav-toggle','nav-menu')
+
+/* =====ACTIVE AND REMOVE MENU===== */
+
+
+
+/* =====SCROLL REVEAL ANIMATION===== */
+
+
+
+/* =====MENU SHOW===== */
+
+
+
+/* =====SCROLL HOME===== */
+
+
+
+
+/* =====SCROLL ABOUT===== */
+
+
+
+
+/* =====SCROLL SKILLS===== */
+
+
+
+
+/* =====SCROLL WORK===== */
+
+
+
+
+/* =====SCROLL CONTACT===== */
+
+
+
+
+/* =====MENU SHOW===== */
+
+
+
+
+/* =====MENU SHOW===== */
+
+
+
+
+/* =====MENU SHOW===== */
