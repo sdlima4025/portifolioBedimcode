31806e2133987e7ad8737905bfb1aca6bcaaec13
Sérgio Lima <sergiolimafg3@gmail.com>
2023-02-23 20:58:12 -0300

    atualizad  seção rede social

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,19 @@ const showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle','nav-menu')
 
 /* =====ACTIVE AND REMOVE MENU===== */
+const navLink =document.querySelectorAll('.nav__link')
 
+    function linkAction() {
+        // Active Link
+        navLink.forEach(n => n.classList.remove('active'))
+            this.classList.add('active')
+
+        // Remove menu mobile
+        const navMenu = document.getElementById('nav-menu')  
+        navMenu.classList.remove('show')  
+    }
+
+    navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /* =====SCROLL REVEAL ANIMATION===== */
